7271d7a1fb446992ccd93d82d0efd248399ba222
aleanrocha <aleanrocha.dev@gmail.com>
2025-01-17 10:48:15 -0300

    constructor

--- src/oo/class_2.ts
+++ src/oo/class_2.ts
@@ -2,22 +2,25 @@ class Course {
   private _id: number | undefined
   public name: string | undefined
 
-  constructor(id: number) {
-    this._id = id
-  }
-
   get id() {
     return this._id
   }
+
+  set id(id: number | undefined) {
+    this._id = id
+  }
 }
 
-const c1 = new Course(Number((Math.random()).toString().slice(2)))
+const c1 = new Course()
 
 console.log(c1)
 
 c1.name = 'Curso de TypeScript.'
+c1.id = 123
 
 console.log(c1)
 console.log(c1.name)
 
 console.log(c1.id)
+
+export {}

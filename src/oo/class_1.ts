75be0ffb9b12dd66529b28c0a9e516208b07a295
aleanrocha <aleanrocha.dev@gmail.com>
2025-01-17 09:24:26 -0300

    atributo privado

--- src/oo/class_1.ts
+++ src/oo/class_1.ts
@@ -1,5 +1,14 @@
 class Course {
-  name: string | undefined
+  private _id: number | undefined
+  public name: string | undefined
+
+  get id() {
+    return this._id
+  }
+
+  set id(id: number | undefined) {
+    this._id = id
+  }
 }
 
 const c1 = new Course()
@@ -7,6 +16,9 @@ const c1 = new Course()
 console.log(c1)
 
 c1.name = 'Curso de TypeScript.'
+c1.id = 123
 
 console.log(c1)
 console.log(c1.name)
+
+console.log(c1.id)
